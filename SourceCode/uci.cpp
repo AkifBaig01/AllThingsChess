46476ffea962c4667dbae55b85f591171149cfd8
AkifBaig01 <81372011+AkifBaig01@users.noreply.github.com>
2025-07-01 23:10:11 +0100

    Fen parser nearing completion
    Almost completed the function to parse the fen string. Updated relevant structs. and build helper functions. main function to be called is fen parser.

--- SourceCode/uci.cpp
+++ SourceCode/uci.cpp
@@ -6,12 +6,22 @@
 #include <bitset>
 #include <cctype>
 #include <algorithm>
+#include <vector>
 
-/*
-Parsed fen will be stored in a struct and that struct will be passed onto bitboard.cpp
-*/
- struct fen_rep
- {
+// ALL DECLARATIONS
+
+// For piece case tables and indexing for array, provides constant values 
+enum {
+    white_pawns, white_knights, white_bishops, white_rooks, white_queens, white_king,
+    black_pawns, black_knights, black_bishops, black_rooks, black_queens, black_king,
+    empty = -1, piece_no
+};
+
+// Could consider moving these into a utils file 
+
+// Represents the full fen string in a board representation
+ struct fen_rep {
+
     int squares[64];  // hold the values for board sqaures occupied and by what piece
 
     // Castling rights explicitly deifned 
@@ -27,21 +37,15 @@ Parsed fen will be stored in a struct and that struct will be passed onto bitboa
     int to_move = 1; // 1 - white to move, 0 - black to move
 
     fen_rep() {
-        std::fill(squares, squares + 64, -1);
+        std::fill(squares, squares + 64, empty);
     }
  };
 
 
-// For piece case tables and indexing for array
-enum {
-    white_pawns, white_knights, white_bishops, white_rooks, white_queens, white_king,
-    black_pawns, black_knights, black_bishops, black_rooks, black_queens, black_king,
-    piece_no
-};
-
-
-
+// Return piece calues based on enum declartions
 int get_piece_enum(char c) {
+
+    // Jump tables, saves multiple if statements 
     switch (c) { 
 
         // black pieces
@@ -60,13 +64,93 @@ int get_piece_enum(char c) {
         case 'Q': return white_queens;
         case 'K': return white_king;
         
-        default: return -1;
+        default: return empty;
     }
 }
 
-fen_rep parser(std::string fen) {
+
+// Splitting the fen up and storing it in a dynamic array
+// Spits string by a given delimiter
+std::vector<std::string> split(const std::string& lines, char delim = ' '){
+
+    // Intialise vector object each elt stored as string 
+    std::vector<std::string> items;
+
+    // Convert string to stream object to get accesss to processing tools 
+    std::istringstream school(lines);
+
+    // Intialise string variable 
+    std::string token;
+
+    // Goes through the given stream object returns true if char availible untill
+    // Next delim or false if not stores chars in token and returns that
+    // get line requires stream object not stirng 
+    while (std::getline(school, token, delim)) {
+
+        // Add to dynamaic array 
+        items.push_back(token);
+    }
+
+
+    // Return dynamic array
+    return items;
+}
+
+void helper_print(fen_rep& parsed) {
+        for (int rank = 0; rank <8; rank++) {
+
+        // Loop over board files
+            for (int file = 0; file < 8; file++) {
+
+                // convert file and rank into sqaure index
+                int sqaure = rank * 8 + file;
+                
+                // print ranks 
+                if (!file)
+                    printf("%d - ", 8 - rank);
+                
+                printf("%d ", parsed.squares[sqaure]);
+            }
+
+        // Print new line every rank 
+        printf("\n");
+    }
+
+    std::cout << "\n";
+    std::cout << "This is to move " << parsed.to_move << "\n";
+    
+
+    std::cout << "\n";
+    std::cout << "This is for white king side castle " << parsed.white_king_side_castle << "\n";
+    std::cout << "This is for white queen side castle " << parsed.white_queen_side_castle << "\n";
+    std::cout << "This is for black king side castle " << parsed.black_king_side_castle << "\n";
+    std::cout << "This is for black queen side castle " << parsed.black_queen_side_castle << "\n";
+    std::cout << "\n";
+
+    std::cout << "This is for enpassant " << parsed.enpassant_square << "\n";
+    std::cout << "This is for halfmove clock "  << parsed.half_move_clock << "\n";
+    std::cout << "This is for fullmove clock " << parsed.full_move_clock  << "\n";
+
+
+}
+
+int sqaure_to_index(const std::string& square) {
+    int file = square[0] - 'a';
+    int rank = square[1] - '1';
+
+    return rank * 8 + file;
+}
+
+// ALL DECLARATIONS
+
+
+
+
+
+// WORKING PROGRAM
+
+fen_rep board_pos(fen_rep& parsed, std::string fen) {
     // Intialise an object of the defined struct 
-    fen_rep parsed;
     int i = 0;
 
     // Loop through each board square
@@ -84,6 +168,7 @@ fen_rep parser(std::string fen) {
 
         // Start of new rank just continue 
         else if (c == '/') {
+            // did not put i += 1 seems to be working dont know why
             continue;
         }
 
@@ -98,33 +183,109 @@ fen_rep parser(std::string fen) {
 }
 
 
-void helper_print(fen_rep& parsed) {
-        for (int rank = 0; rank <8; rank++) {
-        // Loop over board files
-            for (int file = 0; file < 8; file++) {
-                // convert file and rank into sqaure index
-                int sqaure = rank * 8 + file;
-                
-                // print ranks 
-                if (!file)
-                    printf("%d - ", 8 - rank);
-                
-                printf("%d ", parsed.squares[sqaure]);
-            }
 
-        // Print new line every rank 
-        printf("\n");
+fen_rep to_move(fen_rep& parsed, std::string turn_move) {
+    if (turn_move == "w") {
+        parsed.to_move = 1;
     }
+    else
+        parsed.to_move = 0;
+
+    return parsed;
+}
+
+fen_rep enpassant(fen_rep& parsed, std::string enpassant_square) {
+    if (enpassant_square == "-") {
+        parsed.enpassant_square = -1;
+    }
+    else
+        parsed.enpassant_square = 25;
+    
+    return parsed;
+}
+
+fen_rep castling(fen_rep& parsed, std::string castling_rights) {
+    // Goes through string to find if char exists
+    // if it does returns index where it exists 
+    // otherwise returns npos menaing "not found"
+
+    // white rights
+    parsed.white_king_side_castle = castling_rights.find('K') != std::string::npos;
+    parsed.white_queen_side_castle = castling_rights.find('Q') != std::string::npos;
+    
+    // black rights
+    parsed.black_king_side_castle = castling_rights.find('k') != std::string::npos;
+    parsed.black_queen_side_castle = castling_rights.find('q') != std::string::npos;
+
+    return parsed;
 }
 
 
+fen_rep half_move (fen_rep& parsed, std::string halfmove) {
+    parsed.half_move_clock = std::stoi(halfmove);
+
+    return parsed;
+}
+
+fen_rep full_moves (fen_rep& parsed, std::string fullmoves) {
+    parsed.full_move_clock = std::stoi(fullmoves);
+
+    return parsed;
+}
 
 /*
+fen_rep more_moves (fen_rep& parsed, std::vector<std::string> moremoves) {
+    // Ill do later 
+}
+*/
+
+fen_rep fen_parser(std::vector<std::string> fen_array) {
+    fen_rep board;
+    // Main parser call other helper function 
+    // Aim is to call this one fucntion to setup the final strcut to be passed onto bitbaord.cpp
+    // Want to call the list of functions in an array like python would make this alot cleaer and nicer
+    if (fen_array.size() == 6) {
+        board = board_pos(board, fen_array[0]);
+        board = to_move(board, fen_array[1]);
+        board = castling(board, fen_array[2]);
+        board = enpassant(board, fen_array[3]);
+        board = half_move(board, fen_array[4]);
+        board = full_moves(board, fen_array[5]);
+    }
+
+    else
+        std::cout << "Make sure to add other functins here to it that deals with the addtional moves";
+
+    return board;
+}
+
+
+// WORKING PROGRAM 
+
+
+
+
+// remember to enclose everything in readeable comments to make evrything more intuitive
+
+
+
+
+
+
+
+
 int main() {
-    std::string fen = "8/8/8/3p1b2/8/4N3/2R1P1P1/4Q1k1";
-    fen_rep board = parser(fen);
+    std::string fen = "r3k2r/pppq1ppp/2n2n2/2bp4/3P4/2N1PN2/PPP2PPP/R1BQ1RK1 b - - 5 12";
+    std::vector<std::string> fen_array = split(fen);
+
+    fen_rep board = fen_parser(fen_array);
+
     helper_print(board);
 
     return 0;
 }
-*/
+
+
+// 0 - Board pos, 1 - to move, 2 - castling if none -, enpassant sqaures if none -, 50 move rule, full moves availible 
+// Goal is to make one function to setup the postion and pass it onto the bitboard file to finally build
+// Final Representation
